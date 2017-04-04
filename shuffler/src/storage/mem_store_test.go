60f6190b74f86d785178f63f59bef73736296fce
ukode <ukode@google.com>
2017-04-04 11:47:22 -0700

    Refactor memstore.
    Change-Id: I102079cb16fbc921c340387647c58e5f2e0163aa

--- shuffler/src/storage/mem_store_test.go
+++ shuffler/src/storage/mem_store_test.go
@@ -12,7 +12,7 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
-// Unit tests for testing both MemStore and PersistentStore interfaces.
+// MemStore tests.
 
 package storage
 
