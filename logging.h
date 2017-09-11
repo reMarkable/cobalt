471828ce53ff73b205908dd123f192251eccd407
George Kulakowski <kulakowski@google.com>
2017-09-11 14:33:40 -0700

    Rename ftl to fxl
    Change-Id: Icdc360437ad9b6e5fc7f8b327c41dbf8726db1f0

--- logging.h
+++ logging.h
@@ -8,18 +8,18 @@
 #ifdef HAVE_GLOG
 #include <glog/logging.h>
 #elif defined(__Fuchsia__)
-#include "lib/ftl/logging.h"
+#include "lib/fxl/logging.h"
 
-#define VLOG(verboselevel) FTL_VLOG(verboselevel)
-#define LOG(level) FTL_LOG(level)
+#define VLOG(verboselevel) FXL_VLOG(verboselevel)
+#define LOG(level) FXL_LOG(level)
 
-#define CHECK(condition) FTL_CHECK(condition)
-#define CHECK_EQ(val1, val2) FTL_CHECK((val1 == val2))
-#define CHECK_NE(val1, val2) FTL_CHECK((val1 != val2))
-#define CHECK_LE(val1, val2) FTL_CHECK((val1 <= val2))
-#define CHECK_LT(val1, val2) FTL_CHECK((val1 < val2))
-#define CHECK_GE(val1, val2) FTL_CHECK((val1 >= val2))
-#define CHECK_GT(val1, val2) FTL_CHECK((val1 > val2))
+#define CHECK(condition) FXL_CHECK(condition)
+#define CHECK_EQ(val1, val2) FXL_CHECK((val1 == val2))
+#define CHECK_NE(val1, val2) FXL_CHECK((val1 != val2))
+#define CHECK_LE(val1, val2) FXL_CHECK((val1 <= val2))
+#define CHECK_LT(val1, val2) FXL_CHECK((val1 < val2))
+#define CHECK_GE(val1, val2) FXL_CHECK((val1 >= val2))
+#define CHECK_GT(val1, val2) FXL_CHECK((val1 > val2))
 
 #else
 #error "Either HAVE_GLOG or __Fuchsia__ must be defined"
