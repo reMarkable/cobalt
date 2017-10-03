5b0b846a6175212b77ef014b471eaf0c54d31bb0
Alexandre Zani <azani@google.com>
2017-10-03 12:49:17 -0700

    Parse metric, encoding and report configuration files from YAML to proto.
    No actual file IO yet. We're still dealing with strings.
    
    Change-Id: Ib4e6f47a0be9321773f163f40fe72d7d43cd1095

--- config/config_parser/src/config_parser/project_config.go
+++ config/config_parser/src/config_parser/project_config.go
@@ -12,17 +12,65 @@
 // See the License for the specific language governing permissions and
 // limitations under the License.
 
+// This file contains the respresentation for the configuration of a cobalt
+// project (See projectConfig) and a way to parse that configuration information
+// from a yaml string.
+
 package config_parser
 
-import "config"
+import (
+	"config"
+	"fmt"
+	"yamlpb"
+)
 
+// Represents the configuration of a single project.
 type projectConfig struct {
-	customerName    string
-	customerId      uint32
-	projectName     string
-	projectId       uint32
-	contact         string
-	metricConfigs   []config.Metric
-	encodingConfigs []config.EncodingConfig
-	reportConfigs   []config.ReportConfig
+	customerName  string
+	customerId    uint32
+	projectName   string
+	projectId     uint32
+	contact       string
+	projectConfig config.CobaltConfig
+}
+
+// Parse the configuration for one project from the yaml string provided into
+// the config field in projectConfig.
+func parseProjectConfig(y string, c *projectConfig) (err error) {
+	if err := yamlpb.UnmarshalString(y, &c.projectConfig); err != nil {
+		return fmt.Errorf("Error while parsing yaml: %v", err)
+	}
+
+	// TODO(azani): Validate that metrics and encodings referred to exist.
+	metricIds := map[uint32]bool{}
+	encodingIds := map[uint32]bool{}
+	reportIds := map[uint32]bool{}
+	for i, e := range c.projectConfig.EncodingConfigs {
+		if encodingIds[e.Id] {
+			return fmt.Errorf("Encoding id '%v' is repeated in encoding config entry number %v. Encoding ids must be unique.", e.Id, i)
+		}
+		encodingIds[e.Id] = true
+		e.CustomerId = c.customerId
+		e.ProjectId = c.projectId
+	}
+
+	for i, e := range c.projectConfig.MetricConfigs {
+		if metricIds[e.Id] {
+			return fmt.Errorf("Metric id '%v' is repeated in metric config entry number %v. Metric ids must be unique.", e.Id, i)
+		}
+		metricIds[e.Id] = true
+		e.CustomerId = c.customerId
+		e.ProjectId = c.projectId
+	}
+
+	for i, e := range c.projectConfig.ReportConfigs {
+		if reportIds[e.Id] {
+			return fmt.Errorf("Report id '%v' is repeated in report config entry number %v. Report ids must be unique.", e.Id, i)
+		}
+		reportIds[e.Id] = true
+		e.CustomerId = c.customerId
+		e.ProjectId = c.projectId
+	}
+
+	return nil
 }
---
+++ config/config_parser/src/config_parser/project_config_test.go
@@ -0,0 +1,203 @@
+// Copyright 2017 The Fuchsia Authors
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+//    http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package config_parser
+
+import (
+	"config"
+	proto "github.com/golang/protobuf/proto"
+	"reflect"
+	"testing"
+)
+
+// Basic test for parseProjectConfig.
+func TestParseProjectConfig(t *testing.T) {
+	y := `
+metric_configs:
+- id: 1
+  name: metric_name
+  time_zone_policy: UTC
+- id: 2
+  name: other_metric_name
+  time_zone_policy: LOCAL
+encoding_configs:
+- id: 1
+  basic_rappor:
+    prob_0_becomes_1: 0.5
+    prob_1_stays_1: 0.5
+- id: 2
+report_configs:
+- id: 1
+  metric_id: 5
+- id: 2
+`
+	c := projectConfig{
+		customerId: 1,
+		projectId:  10,
+	}
+
+	if err := parseProjectConfig(y, &c); err != nil {
+		t.Error(err)
+	}
+
+	e := config.CobaltConfig{
+		EncodingConfigs: []*config.EncodingConfig{
+			&config.EncodingConfig{
+				CustomerId: 1,
+				ProjectId:  10,
+				Id:         1,
+				Config: &config.EncodingConfig_BasicRappor{
+					&config.BasicRapporConfig{
+						Prob_0Becomes_1: 0.5,
+						Prob_1Stays_1:   0.5,
+					},
+				},
+			},
+			&config.EncodingConfig{
+				CustomerId: 1,
+				ProjectId:  10,
+				Id:         2,
+			},
+		},
+		MetricConfigs: []*config.Metric{
+			&config.Metric{
+				CustomerId:     1,
+				ProjectId:      10,
+				Id:             1,
+				Name:           "metric_name",
+				TimeZonePolicy: config.Metric_UTC,
+			},
+			&config.Metric{
+				CustomerId:     1,
+				ProjectId:      10,
+				Id:             2,
+				Name:           "other_metric_name",
+				TimeZonePolicy: config.Metric_LOCAL,
+			},
+		},
+		ReportConfigs: []*config.ReportConfig{
+			&config.ReportConfig{
+				CustomerId: 1,
+				ProjectId:  10,
+				Id:         1,
+				MetricId:   5,
+			},
+			&config.ReportConfig{
+				CustomerId: 1,
+				ProjectId:  10,
+				Id:         2,
+			},
+		},
+	}
+
+	if !reflect.DeepEqual(e, c.projectConfig) {
+		t.Errorf("%v\n!=\n%v", proto.MarshalTextString(&e), proto.MarshalTextString(&c.projectConfig))
+	}
+}
+
+// Tests that we catch non-unique metric ids.
+func TestParseProjectConfigUniqueMetricIds(t *testing.T) {
+	y := `
+metric_configs:
+- id: 1
+  name: metric_name
+  time_zone_policy: UTC
+- id: 1
+  name: other_metric_name
+  time_zone_policy: LOCAL
+encoding_configs:
+- id: 1
+  basic_rappor:
+    prob_0_becomes_1: 0.5
+    prob_1_stays_1: 0.5
+- id: 2
+report_configs:
+- id: 1
+  metric_id: 5
+- id: 2
+`
+
+	c := projectConfig{
+		customerId: 1,
+		projectId:  10,
+	}
+
+	if err := parseProjectConfig(y, &c); err == nil {
+		t.Error("Accepted non-unique metric id.")
+	}
+}
+
+// Tests that we catch non-unique encoding ids.
+func TestParseProjectConfigUniqueEncodingIds(t *testing.T) {
+	y := `
+metric_configs:
+- id: 1
+  name: metric_name
+  time_zone_policy: UTC
+- id: 2
+  name: other_metric_name
+  time_zone_policy: LOCAL
+encoding_configs:
+- id: 1
+  basic_rappor:
+    prob_0_becomes_1: 0.5
+    prob_1_stays_1: 0.5
+- id: 1
+report_configs:
+- id: 1
+  metric_id: 5
+- id: 2
+`
+
+	c := projectConfig{
+		customerId: 1,
+		projectId:  10,
+	}
+
+	if err := parseProjectConfig(y, &c); err == nil {
+		t.Error("Accepted non-unique encoding id.")
+	}
+}
+
+// Tests that we catch non-unique report ids.
+func TestParseProjectConfigUniqueReportIds(t *testing.T) {
+	y := `
+metric_configs:
+- id: 1
+  name: metric_name
+  time_zone_policy: UTC
+- id: 2
+  name: other_metric_name
+  time_zone_policy: LOCAL
+encoding_configs:
+- id: 1
+  basic_rappor:
+    prob_0_becomes_1: 0.5
+    prob_1_stays_1: 0.5
+- id: 2
+report_configs:
+- id: 1
+  metric_id: 5
+- id: 1
+`
+
+	c := projectConfig{
+		customerId: 1,
+		projectId:  10,
+	}
+
+	if err := parseProjectConfig(y, &c); err == nil {
+		t.Error("Accepted non-unique report id.")
+	}
+}
