41020aea620fd0048c4cebc8810a12ddd2a04335
Zach Bush <zmbush@google.com>
2018-07-25 10:39:25 -0700

    [config] Add validation for expires_after
    CB-147
    
    Change-Id: Idd2b09fb48710f04bbef77c5af12a340868a8a6e

--- config/config_parser/src/config_validator/metrics.go
+++ config/config_parser/src/config_validator/metrics.go
@@ -7,7 +7,9 @@ package config_validator
 import (
 	"config"
 	"fmt"
+	"github.com/golang/glog"
 	"regexp"
+	"time"
 )
 
 // Note that when reports are serialized to CSV, the column headers used are
@@ -30,7 +32,7 @@ func validateConfiguredMetrics(config *config.CobaltConfig) (err error) {
 		metricIds[metricKey] = true
 
 		if err = validateMetric(metric); err != nil {
-			return fmt.Errorf("Error validating metric %v (%v): %v", metric.Name, metric.Id, err)
+			return fmt.Errorf("Error validating metric %v (%v, %v, %v): %v", metric.Name, metric.CustomerId, metric.ProjectId, metric.Id, err)
 		}
 	}
 	return nil
@@ -39,6 +41,29 @@ func validateConfiguredMetrics(config *config.CobaltConfig) (err error) {
 func validateMetric(m *config.Metric) (err error) {
 	if m.Id == 0 {
 		return fmt.Errorf("Metric id '0' is invalid.")
+	}
+
+	if m.ProjectId >= 100 {
+		if m.GetMetaData() == nil || m.GetMetaData().ExpiresAfter == "" {
+			return fmt.Errorf("expires_after is not present. All metrics with project_id > 100 must have an expires_after field set.")
+		}
+
+		oldestValidExpiry := time.Now().AddDate(1, 0, 0)
+		date, err := time.Parse("2006/01/02", m.GetMetaData().ExpiresAfter)
+		if err != nil {
+			return fmt.Errorf("Unable to parse expires_after. Format should be yyyy/mm/dd. %v", err)
+		}
+
+		if date.After(oldestValidExpiry) {
+			return fmt.Errorf("Expiry date '%v' is past the maximum expiry date of '%v'", date, oldestValidExpiry)
+		}
+
+		// We don't currently enforce expiry dates in code, but we should warn about it.
+		if date.Before(time.Now()) {
+			glog.Warningf("Metric '%v' (Customer %v, Project %v, Id %v) has expired.", m.Name, m.CustomerId, m.ProjectId, m.Id)
+		} else if date.Before(time.Now().AddDate(0, 3, 0)) {
+			glog.Warningf("Metric '%v' (Customer %v, Project %v, Id %v) will expire within 3 months.", m.Name, m.CustomerId, m.ProjectId, m.Id)
+		}
 	}
 
 	for name, v := range m.Parts {
